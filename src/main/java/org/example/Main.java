f86e92e8e3dbe41cf51ef4ab5c55ed677ded92f6
11hussain <113888282+11hussain@users.noreply.github.com>
2022-11-21 12:11:06 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("pull to inteliji");
+        
+        System.out.println("pull to 3");
     }
 }
